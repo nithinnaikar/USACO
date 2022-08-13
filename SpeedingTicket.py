186f3bb3ec4b6568c9be78ae5eb85d70d341b691
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-08-13 17:36:39 -0400

    Add files via upload

---
+++ SpeedingTicket.py
@@ -0,0 +1,36 @@
+# Source: https://usaco.guide/general/io
+
+import sys
+
+sys.stdin = open("speeding.in", "r")
+sys.stdout = open("speeding.out", "w")
+
+n, m = map(int, input().split())
+
+speed_limits = []
+
+for _ in range(n):
+    length, speed_limit = map(int, input().split())
+    for _ in range(length):
+        speed_limits.append(speed_limit)
+
+bessie_speeds = []
+
+for _ in range(m):
+    length, bessie_speed = map(int, input().split())
+    for _ in range(length):
+        bessie_speeds.append(bessie_speed)
+
+speed_differences = []
+
+for speed_limit, bessie_speed in zip(speed_limits, bessie_speeds):
+    speed_difference = bessie_speed - speed_limit
+    speed_differences.append(speed_difference)
+
+answer = max(speed_differences)
+
+if answer <= 0:
+    print(0)
+else:
+    print(answer)
+
