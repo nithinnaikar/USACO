0402fede46ab36e569afe7f164aab452623773c3
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-08-15 17:44:10 -0400

    Add files via upload

---
+++ maximumdistance.py
@@ -0,0 +1,25 @@
+# Source: https://usaco.guide/general/io
+import math
+
+n = int(input())
+
+x_coords = list(map(int, input().split()))
+y_coords = list(map(int, input().split()))
+points = [(x_coord, y_coord) for x_coord, y_coord in zip(x_coords, y_coords)]
+
+def euclidean_distance(point_1, point_2):
+    return math.sqrt((point_2[0] - point_1[0])**2  + (point_2[1] - point_1[1])**2)
+
+high = 0
+
+for point_1 in points: 
+    for point_2 in points: 
+        squared_euclidean_distance = euclidean_distance(point_1, point_2)**2
+        if squared_euclidean_distance > high:
+            high = squared_euclidean_distance 
+        else:
+            pass
+
+
+print(round(high))
+
