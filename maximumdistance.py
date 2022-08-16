5c1bc0f61927ac3efb186187c924d5996fb99aca
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-08-16 15:57:02 -0400

    Add files via upload

--- maximumdistance.py
+++ maximumdistance.py
@@ -1,7 +1,6 @@
 # Source: https://usaco.guide/general/io
 import math
 
-
 n = int(input())
 
 x_coords = list(map(int, input().split()))
@@ -12,8 +11,6 @@ def euclidean_distance(point_1, point_2):
     return math.sqrt((point_2[0] - point_1[0])**2  + (point_2[1] - point_1[1])**2)
 
 high = 0
-
-# This nested loop will perform more checks than necessary but that is fine for this problem
 
 for point_1 in points: 
     for point_2 in points: 
