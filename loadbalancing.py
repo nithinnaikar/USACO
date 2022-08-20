16d7b57fab32640205274ad0476c8af1926a0a75
nithinnaikar <62145888+nithinnaikar@users.noreply.github.com>
2022-08-20 18:47:38 -0400

    Update loadbalancing.py

--- loadbalancing.py
+++ loadbalancing.py
@@ -1,5 +1,6 @@
 # Source: https://usaco.guide/general/io
-# Program only passes 5/10 test cases due to TLE. Efficient solution also posted. 
+# Program only passes 5/10 test cases due to TLE. Can make program more efficient by limiting search space to only coordinates that are close to the average coordinate
+# all cows. 
 import sys
 
 sys.stdin = open("balancing.in", "r")
@@ -17,11 +18,19 @@ ms = []
 
 for a in range(0, B + 2, 2):
     for b in range(0, B + 2, 2):
-        quadrant_1_cows = [cow for cow in cow_coordinates if cow[0] > a and cow[1] > b]
-        quadrant_2_cows = [cow for cow in cow_coordinates if cow[0] < a and cow[1] > b]
-        quadrant_3_cows = [cow for cow in cow_coordinates if cow[0] < a and cow[1] < b]
-        quadrant_4_cows = [cow for cow in cow_coordinates if cow[0] > a and cow[1] < b]
-        m = max(len(quadrant_1_cows), len(quadrant_2_cows), len(quadrant_3_cows), len(quadrant_4_cows))
+        quadrant_1_cows, quadrant_2_cows, quadrant_3_cows, quadrant_4_cows = 0, 0, 0, 0
+        for cow in cow_coordinates:
+            if cow[0] > a and cow[1] > b:
+                quadrant_1_cows += 1
+            elif cow[0] < a and cow[1] > b:
+                quadrant_2_cows += 1
+            elif cow[0] < a and cow[1] < b:
+                quadrant_3_cows += 1
+            else:
+                quadrant_4_cows += 1
+
+        m = max(quadrant_1_cows, quadrant_2_cows, quadrant_3_cows, quadrant_4_cows)
         ms.append(m)
 
 print(min(ms))
+
